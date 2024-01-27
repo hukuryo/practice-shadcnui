f81b5899c94766040fb534f29156934dffc687d0
hukuryo <ryohei.1998.05.09@gmail.com>
2024-01-27 13:51:22 +0900

    アコーディングを追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,29 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 function App() {
   return (
     <>
-      <div>
+      <div className="mb-10">
+        <p>Button</p>
         <Button>Click me</Button>
+      </div>
+      <div className="mb-10">
+        <p>Accordion</p>
+        <Accordion type="single" collapsible>
+          <AccordionItem value="item-1">
+            <AccordionTrigger>Is it accessible?</AccordionTrigger>
+            <AccordionContent>
+              Yes. It adheres to the WAI-ARIA design pattern.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
     </>
   );
